eb681461c94d6670d79546c3e206284947e4c1e8
zorkb <zachbuchs@gmail.com>
2021-11-28 22:10:01 -0800

    well schema complete

---
+++ server/models/WellData.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const wellDataSchema = new mongoose.Schema({
+    wellOwner: { type: mongoose.Types.ObjectId, ref: 'WellOwner' },
+    wellName: { type: String, default: 'New Well' },
+    landName: { type: String, required: false },
+    latitude: { type: Number, required: true },
+    longitude: { type: Number, required: true },
+    //add in default image
+    image: { type: String, default:'' },
+    motorMake: { type: String, required: false },
+    pumpMake: { type: String, required: false },
+    meterNumber: { type: Number, required: false },
+    serialNumber: { type: Number, required: false },
+    voltage: { type: Number, required: false },
+    amps: { type: Number, required: false },
+    voltage: { type: Number, required: false },
+    lastChecked: { type: String, required: false },
+    lastRetrofit: { type: String, required: false },
+    lastChecked: { type: String, required: false },
+    dueForCheck: { type: Boolean, required: false },
+    retrofitDesired: { type: Boolean, required: false },
+    retrofitScheduled: { type: String, required: false },
+    },
+    { timestamps: true },
+    { collection: 'well-data' },
+);
+
+const WellData = mongoose.model('WellData', wellDataSchema);
+
+module.exports = WellData
