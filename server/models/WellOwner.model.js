10f52d5bed3248c4a1959706926db6fb0161e0d2
zorkb <zachbuchs@gmail.com>
2021-11-28 23:03:01 -0800

    set up models index and db connection file

--- server/models/WellOwner.model.js
+++ server/models/WellOwner.model.js
@@ -4,13 +4,17 @@ const wellOwnerSchema = new mongoose.Schema({
     company: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-
     firstName: { type: String, required: false },
     lastName: { type: String, required: false },
     streetAddress: { type: String, required: false },
     city: { type: String, required: false },
     state: { type: String, required: false },
     avatar: { type: String, required: false },
+    role: {
+        type: String,
+        required: [true, 'please select an account type'],
+        enum: ['Well Owner', 'Technician']
+    },
     },
     { timestamps: true },
     { collection: 'well-owner-data' },
---
+++ server/models/index.model.js
@@ -0,0 +1,7 @@
+require('../config/db.connection')
+
+module.exports = {
+    WellOwner: require("./WellOwner"),
+    TestData: require("./TestData"),
+    WellData: require("./WellData"),
+};
